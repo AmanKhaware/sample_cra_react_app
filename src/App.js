d9cb296bf47d56d11930fcd1692b51dcb6faa8cd
ESOLUTIONS\Aman.Khaware <Aman.Khaware@trivium-esolutions.com>
2023-05-25 16:32:59 +0530

    removed commented code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Footer from './component/footer';
 import Menu from './component/menu';
 import Content from './component/content';
 
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
 function App() {
   return (
     <div>
